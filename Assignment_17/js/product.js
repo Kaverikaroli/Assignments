0b7833f1348aba65cf1877d1091fafb68447901c
Kaverikaroli <kaverikaroli@gmail.com>
2021-11-19 18:07:41 +0530

    commit

--- Assignment_17/js/product.js
+++ Assignment_17/js/product.js
@@ -1,17 +1,29 @@
-function AppViewModel() {
-    var self = this;
- 
-    self.people = ko.observableArray([
-        { name: 'Bert' },
-        { name: 'Charles' },
-        { name: 'Denise' }
-    ]);
- 
-    self.addPerson = function() {
-        self.people.push({ name: "New at " + new Date() });
-    };
- 
-    self.removePerson = function() {
-        self.people.remove(this);
-    }
+function Product(pname,unitprice){
+
+    this.pname=pname;
+
+    this.unitprice=unitprice;
+
 }
+
+const p1=new Product("Pepsi",20.00);
+
+const p2=new Product("Maaza",20.00);
+
+const p3=new Product("Coca Cola",20.00);
+
+
+
+function ProductListModel(){
+
+    this.productList=ko.observableArray([p1,p2,p3])
+
+    console.log("Product list has "+this.productList());
+
+}
+
+
+
+const productListModel=new ProductListModel();
+
+ko.applyBindings(productListModel);
